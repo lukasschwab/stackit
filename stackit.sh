95b54a19787878935063caf0ee2940ffd7606d2d
Vicki Niu <vicki.niu@gmail.com>
2015-01-31 10:48:10 -0800

    Adding command-line search & error searching functionality

---
+++ stackit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while getopts ":s:" opt; do
+    case "$opt" in
+        s)
+            python stackit-core.py $2
+            ;;
+        \?)
+
+            ERROR=$(($2 $3 >&2) 2>&1)
+
+            echo="ERROR=$ERROR"
+
+            $2 $3 2>&1 test | python stackit-core.py -stderr $ERROR$
+            ;;
+       
+    esac
+done
