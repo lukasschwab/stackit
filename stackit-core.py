99ddd8294a915f28e606f9b594e4efc63694035c
Vicki Niu <vicki.niu@gmail.com>
2015-01-31 01:36:27 -0800

    Implementing quasi-scroll functionality

--- stackit-core.py
+++ stackit-core.py
@@ -8,6 +8,9 @@ from stackexchange import Sort
 
 import requests
 import bs4
+import msvcrt as m
+
+NUM_RESULTS = 5
 
 # HTML to markdown parsing
 # https://github.com/aaronsw/html2text
@@ -16,27 +19,39 @@ h = html2text.HTML2Text()
 
 so = stackexchange.Site(stackexchange.StackOverflow, impose_throttling=True)
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         term = raw_input('Please provide a search term:')
     else:
         term = ' '.join(sys.argv[1:])
     print('Searching for %s... \n' % term,)
-    #sys.stdout.flush()
+    sys.stdout.flush()
 
-    question = so.search_advanced(q = term, sort = Sort.Votes)[0]
+    questions = so.search_advanced(q = term, sort = Sort.Votes)
+    j = 0
+    count = 0
+    while(j < len(questions)):
+        i = 0
+        while(i < NUM_RESULTS):
+            answer = ""
+            question = questions[j]
+            if 'accepted_answer_id' in question.json:
+                i+=1
+                count += 1
+                questionurl = question.json['link']
+                answerid = question.json['accepted_answer_id']
+                #questionurl gives the url of the SO question
+                #the answer is under id "answer-answerid", and text of answer is in class post-text
 
-    if 'accepted_answer_id' in question.json:
-        questionurl = question.json['link']
-        answerid = question.json['accepted_answer_id']
-
-        print ('%8d %s' % (question.id, question.title))
-        #questionurl gives the url of the SO question
-        #the answer is under id "answer-answerid", and text of answer is in class post-text
-
-        # Pulls the html from the StackOverflow site, converts to Beautiful Soup
-        response = requests.get(questionurl)
-        soup = bs4.BeautifulSoup(response.text)
-        # Prints the accepted answer div, concatonated "answer-" and answerid
-        # Gets the p string -- do al answers follow this format, or do some have more info?
-        print(h.handle(soup.find("div", {"id": "answer-"+str(answerid)}).p.prettify()))
+                # Pulls the html from the StackOverflow site, converts to Beautiful Soup
+                response = requests.get(questionurl)
+                soup = bs4.BeautifulSoup(response.text)
+                # Prints the accepted answer div, concatonated "answer-" and answerid
+                # Gets the p string -- do al answers follow this format, or do some have more info?
+                print(str(count) + "\n" + "Question: " + question.title + "\nAnswer: " + h.handle(soup.find("div", {"id": "answer-"+str(answerid)}).p.prettify()) + "\n")
+            j += 1
+        more = raw_input("Press m for more, or a number to select: ")
+        if(more == 'm'):
+            continue
+        print(more)
