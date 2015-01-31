11d848042f03c5a182f2e4c31ad1e7b08e051cd8
Vicki Niu <vicki.niu@gmail.com>
2015-01-30 21:47:17 -0800

    Sorting questions returned by number of votes

--- stackit-core.py
+++ stackit-core.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import sys
 import stackexchange
+from stackexchange import Sort
 
 so = stackexchange.Site(stackexchange.StackOverflow, impose_throttling=True)
 
@@ -13,12 +14,12 @@ if __name__ == '__main__':
     print('Searching for %s...' % term,)
     sys.stdout.flush()
 
-    questions = so.search(intitle=term)
+    questions = so.search(sort = Sort.Votes, intitle=term)
 
     for question in questions:
         if 'accepted_answer_id' in question.json:
-        	questionurl = question.json['link']
-        	answerid = question.json['accepted_answer_id']
-        	print('%8d %s\nAnswer ID %d' % (question.id, question.title, answerid))
-        	#questionurl gives the url of the SO question
-        	#the answer is under id "answer-answerid", and text of answer is in class post-text
+            questionurl = question.json['link']
+            answerid = question.json['accepted_answer_id']
+            print('%d %8d %s\nAnswer ID %d' % (question.score, question.id, question.title, answerid))
+            #questionurl gives the url of the SO question
+            #the answer is under id "answer-answerid", and text of answer is in class post-text
