755eced150914bd5df72d6788ad132b9681a042d
Lukas Schwab <lukas.schwab@gmail.com>
2015-02-03 15:07:27 -0800

    Inserts backslash in CWD to not hit space error

--- stackit/stackit_core.py
+++ stackit/stackit_core.py
@@ -98,7 +98,8 @@ def getTerm(parser):
     if(pArgs.stderr):
         commandlist = pArgs.stderr.split()
         command = commandlist[0]
-        filename = os.getcwd() + "/" + commandlist[1]
+        # Get current working directory and replace spaces with '\ ' to stop errors
+        filename = (os.getcwd()).replace(' ','\ ') + "/" + commandlist[1]
         process = subprocess.Popen(command + " " + filename, stderr=subprocess.PIPE, shell=True)
         output = process.communicate()[1]
         term += (output.splitlines()[-1] + " ")
